fdbe05484c9a82efaf409f41068409a86a3dfdb5
shahid <shahidangus@gmail.com>
2025-02-08 01:15:48 +0530

    added two more function which is sending otp and verifying otp with row form i am taking userId with user itself but this is not a optimal way do things lets optimize this using jsonwebtoken

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -48,9 +48,9 @@ export const register = async (req, res) => {
 
         const mailerOption = {
             from: process.env.SENDER_EMAIL,
-            to:createUser.email,
-            subject:"Welcome to CoderBoyz.com",
-            text:`Welcome to CoderBoyz.com website. Your account has been created with this email: ${createUser.email}`
+            to: createUser.email,
+            subject: "Welcome to CoderBoyz.com",
+            text: `Welcome to CoderBoyz.com website. Your account has been created with this email: ${createUser.email}`
         }
 
         try {
@@ -58,7 +58,7 @@ export const register = async (req, res) => {
             console.log("Welcome email sent successfully");
         } catch (emailError) {
             console.error("Error sending email:", emailError.message);
-            return res.json({ success: false, message: "Error sending welcome email" });
+            return res.json({success: false, message: "Error sending welcome email"});
         }
 
         return res.json({success: true, message: "User created successful", createUser});
@@ -122,10 +122,79 @@ export const logout = async (req, res) => {
         return res.json({success: true, message: "Logout successful"});
 
     } catch (errors) {
-        return res.json({success: false, message: errors.message, type: "Internal Server Error"})
+        return res.json({success: false, message: errors.message, type: "Internal Server Error"});
     }
 }
 
 
+export const sendingVerifyOtp = async (req, res) => {
+    const {userId} = req.body;
+    if (!userId) return res.json({success: false, message: "Something went wrong! Invalid Id"});
+
+    try {
+
+        const existingUser = await userModel.findById(userId);
+        if (existingUser.isAccountVerified) return res.json({success: false, message: "Already verified"});
+
+        const OTP = String(Math.floor(100000 + Math.random() * 900000));
+        existingUser.verifyOtp = OTP;
+        existingUser.verifyOtpExpireAt = Date.now() + 24 * 60 * 60 * 1000;
+        await existingUser.save();
+
+        const mailerOption = {
+            from: process.env.SENDER_EMAIL,
+            to: existingUser.email,
+            subject: "Welcome to CoderBoyz.com",
+            text: `Welcome to CoderBoyz.com website. Your email verification OTP is ${OTP} verify your email with this otp`
+        }
+
+        try {
+            await transporter.sendMail(mailerOption);
+            console.log("OTP send successful");
+        } catch (emailError) {
+            console.error("Error sending OTP in email:", emailError.message);
+            return res.json({success: false, message: "Error sending OTP email"});
+        }
+
+        return res.json({success: true, message: "OTP send success"});
 
 
+    } catch (errors) {
+        return res.json({success: false, message: errors.message, type: "Internal Server Error"});
+    }
+}
+
+export const verifyEmail = async (req, res) => {
+    const {userId, userOtp} = req.body;
+
+    if (!userId || !userOtp) return res.json({success: false, message: "Invalid OTP"});
+
+    try {
+
+        const user = await userModel.findById(userId);
+        if (!user) return res.json({success: false, message: "No UserFound!"});
+
+        if (user.isAccountVerified) return res.json({success: false, message: "Already verified!"});
+
+        if (user.verifyOtp === userOtp) {
+            if (user.verifyOtpExpireAt < Date.now()) return res.json({
+                success: false,
+                message: "Otp is expired request for new!"
+            });
+            user.isAccountVerified = true;
+            user.verifyOtp = "";
+            user.verifyOtpExpireAt = 0;
+            await user.save();
+
+        } else {
+            return res.json({success: false, message: "Invalid otp try again"});
+        }
+
+        return res.json({success: true, message: "OTP verified success"});
+
+    } catch (errors) {
+        return res.json({success: false, message: errors.message, type: "Internal Server Error"});
+    }
+
+
+}
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,13 +1,15 @@
 import express from 'express';
-import {register, login, logout} from "../controller/auth.controller.js";
+import {register, login, logout, sendingVerifyOtp, verifyEmail} from "../controller/auth.controller.js";
 
 
 const router = express.Router();
 
 
-router.post("/register", register)
-router.post("/login", login)
-router.get("/logout", logout)
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
+router.post("/get-otp", sendingVerifyOtp);
+router.post("/verify", verifyEmail);
 
 
 export default router;
